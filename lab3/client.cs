9c9b47d9daa90b35d1c5786fc6047f91f111bcbb
ndhoang283 <21520869@gm.uit.edu.vn>
2023-04-12 09:46:14 +0700

    lab 3 ltht

---
+++ lab3/client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace lab3
+{
+    public partial class client : Form
+    {
+        public client()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UdpClient udpClient = new UdpClient();
+            Byte[] sendBytes = Encoding.ASCII.GetBytes("Hello World?");
+            udpClient.Send(sendBytes, sendBytes.Length, tbHost.Text, 8080);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ lab3/sever.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+
+namespace lab3
+{
+    public partial class sever : Form
+    {
+        public sever()
+        {
+            InitializeComponent();
+            Thread thdUDPSever = new Thread(new ThreadStart(severThread));
+        }
+        public void InfoMessage(string mess)
+        {
+            ListViewItem lv = new ListViewItem();
+            lv.Text = mess;
+            listView1.Items.Add(lv);
+        }
+
+        public void severThread()
+        {
+            UdpClient udpClient = new UdpClient(8080);
+            while (true) { 
+            IPEndPoint RemotelpEndPoint = new IPEndPoint(IPAddress.Any,0);
+            Byte[] receiveBytes=udpClient.Receive(ref RemotelpEndPoint);
+            string returnData =Encoding.ASCII.GetString(receiveBytes);
+            string mess= RemotelpEndPoint.Address.ToString()+":"+ returnData.ToString();
+            InfoMessage(mess);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
